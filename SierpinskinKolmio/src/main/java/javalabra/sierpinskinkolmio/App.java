51a2a3ad3ebb4a628c0bcf7ade5cfea988fdc89c
ReconA <waldenatte@gmail.com>
2014-01-16 13:08:57 +0200

    Itse projekti lisatty

---
+++ SierpinskinKolmio/src/main/java/javalabra/sierpinskinkolmio/App.java
@@ -0,0 +1,13 @@
+package javalabra.sierpinskinkolmio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
