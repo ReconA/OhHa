348637fde39d2adcb4afa329a1c169380526163a
ReconA <waldenatte@gmail.com>
2014-02-06 18:32:10 +0200

    Javadocia

--- SierpinskinKolmio/src/main/java/Main/Main.java
+++ SierpinskinKolmio/src/main/java/Main/Main.java
@@ -1,3 +1,5 @@
+package Main;
+
 
 import javalabra.piirtaja.Piirtaja;
 import javalabra.kayttoliittyma.Kayttoliittyma;
--- SierpinskinKolmio/src/test/java/javalabra/logiikka/SijainninLaskijaTest.java
+++ SierpinskinKolmio/src/test/java/javalabra/logiikka/SijainninLaskijaTest.java
@@ -23,45 +23,45 @@ public class SijainninLaskijaTest {
 
     }
 
-//    @Test
-//    public void konstruktoriSaaTyhjanKolmionAlkupisteOrigo() {
-//        Kolmio tyhjaKolmio = new Kolmio();
-//        SijainninLaskija tyhjaLaskija = new SijainninLaskija(tyhjaKolmio);
-//
-//        assertEquals("(0,0)", tyhjaLaskija.getSijainti().toString());
-//    }
-//
-//    @Test
-//    public void laskinLaskeeUudenPisteenOikein() {
-//        Piste sijainti = new Piste(-15, 20);
-//        Piste k = new Piste(40, 0);
-//
-//        laskija.setSijainti(sijainti);
-//        laskija.laskeSeuraavaSijainti();
-//
-//        assertEquals("(13,10)", laskija.getSijainti().toString());
-//    }
-//
-//    @Test
-//    public void uudenPisteenLaskentaToimiiUseastiKutsuttuna() {
-//        Piste sijainti = new Piste(20, -10);
-//        Piste k1 = new Piste(-15, -15);
-//
-//        laskija.setSijainti(sijainti);
-//
-//        laskija.laskeSeuraavaSijainti();
-//        laskija.laskeSeuraavaSijainti();
-//        laskija.laskeSeuraavaSijainti();
-//
-//        assertEquals("(31,12)", laskija.getSijainti().toString());
-//    }
-//    
-//    @Test
-//    public void liikkuminenKohtiTyhjaaKulmaa() {
-//        
-//        Piste tyhja = null;
-//        laskija.laskeSeuraavaSijainti();
-//    }
+    @Test
+    public void konstruktoriSaaTyhjanKolmionAlkupisteOrigo() {
+        Kolmio tyhjaKolmio = new Kolmio();
+        SijainninLaskija tyhjaLaskija = new SijainninLaskija(tyhjaKolmio);
+
+        assertEquals("(0,0)", tyhjaLaskija.getSijainti().toString());
+    }
+
+    @Test
+    public void laskinLaskeeUudenPisteenOikein() {
+        Piste sijainti = new Piste(-15, 20);
+        Piste k = new Piste(40, 0);
+
+        laskija.setSijainti(sijainti);
+        laskija.laskeSeuraavaSijainti();
+
+        assertEquals("(13,10)", laskija.getSijainti().toString());
+    }
+
+    @Test
+    public void uudenPisteenLaskentaToimiiUseastiKutsuttuna() {
+        Piste sijainti = new Piste(20, -10);
+        Piste k1 = new Piste(-15, -15);
+
+        laskija.setSijainti(sijainti);
+
+        laskija.laskeSeuraavaSijainti();
+        laskija.laskeSeuraavaSijainti();
+        laskija.laskeSeuraavaSijainti();
+
+        assertEquals("(31,12)", laskija.getSijainti().toString());
+    }
+    
+    @Test
+    public void liikkuminenKohtiTyhjaaKulmaa() {
+        
+        Piste tyhja = null;
+        laskija.laskeSeuraavaSijainti();
+    }
     
     
     
